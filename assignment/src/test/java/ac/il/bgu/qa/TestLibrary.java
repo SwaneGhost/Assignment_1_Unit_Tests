6c9624ad155a6b6b9e0d557bef5b642e695823c9
Denis Streltsovski <den.stre9@gmail.com>
2024-11-17 18:17:26 +0200

    added 1 test
    understood what made mockito fail

--- assignment/src/test/java/ac/il/bgu/qa/TestLibrary.java
+++ assignment/src/test/java/ac/il/bgu/qa/TestLibrary.java
@@ -6,5 +6,30 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.mockito.*;
 
+// Test Naming Convention: Given<Condition>_When<Method>_Then<Result>
+// Using JAVA - 19.0.2 by Amazon Corretto
+
 public class TestLibrary {
+
+    @Mock
+    private DatabaseService databaseService;
+
+    @Mock
+    private ReviewService reviewService;
+
+    @InjectMocks
+    private Library library;
+
+    private Book book;
+
+    @BeforeEach
+    public void setUp() {
+        library = new Library(databaseService, reviewService);
+        book = new Book("978-3-16-148410-0", "The Catcher in the Rye", "J.D. Salinger");
+    }
+
+    @Test
+    public void GivenNull_WhenAddBook_ThenIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(null));
+    }
 }
